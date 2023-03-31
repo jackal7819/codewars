494576623c7111a01c1633be6a220403afa58c7a
“jackal7819” <jackal3166@gmail.com>
2023-03-31 18:09:39 +0300

    Create I love you, a little , a lot, passionately ... not at all.js

---
+++ Javascript/8kyu/I love you, a little , a lot, passionately ... not at all/I love you, a little , a lot, passionately ... not at all.js
@@ -0,0 +1,11 @@
+function howMuchILoveYou(nbPetals) {
+    const arr = [
+        'I love you',
+        'a little',
+        'a lot',
+        'passionately',
+        'madly',
+        'not at all',
+    ];
+    return arr[(nbPetals - 1) % 6];
+}
