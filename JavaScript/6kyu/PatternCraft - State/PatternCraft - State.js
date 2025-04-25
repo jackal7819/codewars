5fd7e80b97d339f0d1334f2a8f3444ba4179656f
jackal7819 <jackal3166@gmail.com>
2025-04-25 21:36:22 +0300

    feat: solve 6kyu Kata: PatternCraft - State

---
+++ JavaScript/6kyu/PatternCraft - State/PatternCraft - State.js
@@ -0,0 +1,27 @@
+class SiegeState {
+	constructor() {
+		this.canMove = false;
+		this.damage = 20;
+	}
+}
+
+class TankState {
+	constructor() {
+		this.canMove = true;
+		this.damage = 5;
+	}
+}
+
+class Tank {
+	constructor() {
+		this.state = new TankState();
+	}
+
+	get canMove() {
+		return this.state.canMove;
+	}
+
+	get damage() {
+		return this.state.damage;
+	}
+}
